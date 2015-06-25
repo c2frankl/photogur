2be28afc7cfaabbd3c6bb756d00bc5e0c4d6a5a0
Craig Franklin <craigfranklin@Craigs-MacBook-Pro-2.local>
2015-06-25 16:43:13 -0400

    Making each picture show at its own URL with dynamic routes

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,7 +1,13 @@
 class PicturesController < ApplicationController
+
     def index
         @pictures = Picture.all
     end
+
+    def show
+        @picture = Picture.find(params[:id])
+    end
+
 end
 
 #This is the controller
